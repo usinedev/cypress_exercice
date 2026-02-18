cbff6741e47c47299b6fe8801e692b86bc9d63e1
Sacha <124590090+usinedev@users.noreply.github.com>
2026-02-18 09:56:06 +0100

    projectId addded

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   allowCypressEnv: false,
 
+  projectId: "sfsp2y",
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
