51e1ce2e73ca9418e1014a320d93e75df4d294c2
Sacha <124590090+usinedev@users.noreply.github.com>
2026-02-18 09:30:25 +0100

    first commit

---
+++ cypress/e2e/tp_2.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+describe('template spec', () => {
+    it('passes', () => {
+    cy.visit('http://localhost:3000/');
+
+    cy.get(`[name="username"]`).type('Usertest');
+
+    cy.get(`[name="password"]`).type('1234');
+
+    cy.get(`button`).click();
+});
+});
